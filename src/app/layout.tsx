9ff3e5590a7f36d42fc16fbfbb9dad6d59658bc0
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-12-06 13:07:52 -0300

    feat: implement card and decoration structures

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Space_Grotesk} from "next/font/google";
 import "./globals.css";
+import "./media.css";
 
 const space_grotesk = Space_Grotesk({
 	display: "swap",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,42 @@
+import Image from "next/image";
+
 const Home: React.FC = () => {
-	return <></>;
+	return (
+		<>
+			<div className="decoration">
+				<div className="card card--front">
+					<header className="card--front__header">
+						<Image
+							src="images/card-logo.svg"
+							alt="card logo"
+							height={40}
+							width={70}
+						/>
+					</header>
+					<footer className="card--front__footer">
+						<div className="card--front__numbers">
+							<span className="number-set">0000</span>
+							<span className="number-set">0000</span>
+							<span className="number-set">0000</span>
+							<span className="number-set">0000</span>
+						</div>
+						<div className="card--front__info">
+							<span className="info__name">jane appleseed</span>
+							<span className="info__expire-date">
+								<span className="expire-date__month">00</span>
+								<span>/</span>
+								<span className="expire-date__year">00</span>
+							</span>
+						</div>
+					</footer>
+				</div>
+				<div className="card card--back">
+					<span className="card--back__cvc">000</span>
+				</div>
+			</div>
+			<form className="form"></form>
+		</>
+	);
 };
 
 export default Home;
